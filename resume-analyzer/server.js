4b32899d23a186a1e89d254cb86d104972a44320
Ahad <work.ahad123@gmail.com>
2026-07-08 00:46:45 +0530

    Fix Linux Filename Issue

--- resume-analyzer/server.js
+++ resume-analyzer/server.js
@@ -14,6 +14,13 @@ app.use("/", analyzeroutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "AI Resume Analyzer Backend is Running 🚀",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
